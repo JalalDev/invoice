4efc91781b81b4074f727e07f8cdff98e28ba3d6
Jalal Dev <grinijalal@yahoo.fr>
2025-11-19 11:50:40 +0100

    Client model

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::resource('clients', ClientController::class);
