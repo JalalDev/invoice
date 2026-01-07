7fd54de8a6aebb7ddbce8534b36b6e77753b8b4c
Jalal Dev <grinijalal@yahoo.fr>
2026-01-07 11:41:55 +0100

    wip auth

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/about', function () {
 Route::get('client/search', [ClientController::class, 'search'])->name('clients.search');
 Route::resource('clients', ClientController::class);
 Route::resource('articles', ArticleController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
